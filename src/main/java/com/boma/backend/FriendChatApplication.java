8e65826440654464796a00c4c5c0070294d84dcf
akins-b <akinsboms@gmail.com>
2025-06-30 09:56:24 +0100

    inital commit

---
+++ src/main/java/com/boma/backend/FriendChatApplication.java
@@ -0,0 +1,13 @@
+package com.boma.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boma/backend/FriendChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boma.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
